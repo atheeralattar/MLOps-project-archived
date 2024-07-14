50b59a9dbca57a040b5cf87640789f7294423984
Atheer Al Attar <atheeralattar@gmail.com>
2024-07-14 15:01:36 -0500

    Working on the modeling

---
+++ src/modeling/modeling.py
@@ -0,0 +1,9 @@
+"""
+This module will perform the following tasks:
+1- Reading prepared dataset from the dataset module.
+2- Generating the train/test datasets (due to the size, csv files will be generated)
+3- Generating DictVectorizer for the the training data
+4- Creating several models and log the accuracy, expirements, models
+"""
+
+def
